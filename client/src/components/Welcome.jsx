e4b8c856556400d46f0397a4da8bbf4c25af845e
CrashedOut93 <crashstocks@gmail.com>
2022-03-22 13:54:33 -0500

    Update Welcome.jsx
    test

--- client/src/components/Welcome.jsx
+++ client/src/components/Welcome.jsx
@@ -17,6 +17,7 @@ const Input = ({ placeholder, name, type, value, handleChange }) => (
     onChange={(e) => handleChange(e, name)}
     className='my-2 w-full rounded-sm p-2 outline-none bg-transparent text-white border-none text-sm white-glassmorphism'
     />
+    //test
 );
 
 function Welcome() {
