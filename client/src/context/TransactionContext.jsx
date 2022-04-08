2be7daa0a4b3a8884301eb7656dbbd616b4c0a81
CrashedOut93 <crashstocks@gmail.com>
2022-04-07 23:01:28 -0500

    Footer update

--- client/src/context/TransactionContext.jsx
+++ client/src/context/TransactionContext.jsx
@@ -33,18 +33,19 @@ export const TransactionProvider = ({ children }) => {
 
             const availableTransactions = await transactionContract.getAllTransactions();
 
-            const structuredTransactions = availableTransactions.map((transaction) => ({
-                addressTo: transaction.receiver, 
-                addressFrom: transaction.sender, 
-                timestamp: new Date(transaction.timestamp.toNumber() * 1000).toLocaleString(),
-                message: transaction.message, 
-                keyword: transaction.keyword, 
-                amount: parseInt(transaction.amount._hex) / (10 ** 18)
-            }))
+            // const structuredTransactions = availableTransactions.map((transaction) => ({
+            //     addressTo: transaction.receiver, 
+            //     addressFrom: transaction.sender, 
+            //     timestamp: new Date(transaction.timestamp.toNumber() * 1000).toLocaleString(),
+            //     message: transaction.message, 
+            //     keyword: transaction.keyword, 
+            //     amount: parseInt(transaction.amount._hex) / (10 ** 18)
+            // }))
 
             console.log(structuredTransactions)
 
             setTransactions(structuredTransactions);
+            console.log(availableTransactions);
         } catch (error) {
             console.log(error);
         }
