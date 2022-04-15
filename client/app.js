465c315338f5463db576c63684745a5446f570b4
CrashedOut93 <crashstocks@gmail.com>
2022-04-14 23:06:30 -0500

    Express

--- client/app.js
+++ client/app.js
@@ -5,7 +5,7 @@ const path = require('path');
 const port = process.env.PORT || 3000;
 
 if (process.env.NODE_ENV === "production"){
-    app.use(express.static());
+    app.use(express.static('build'));
 }
 
 app.listen(port, (err) => {
