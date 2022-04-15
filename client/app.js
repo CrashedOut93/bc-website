01add283b92eb78d43cc944c8ddbde994e91fa19
CrashedOut93 <crashstocks@gmail.com>
2022-04-14 22:36:26 -0500

    Express

---
+++ client/app.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+
+const path = require('path');
+const port = process.env.PORT || 3000;
+
+if (process.env.NODE_ENV === "production"){
+    app.use(express.static());
+}
+
+app.listen(port, (err) => {
+    if (err) return console.log(err);
+    console.log("Server running on port: ", port);
+})
